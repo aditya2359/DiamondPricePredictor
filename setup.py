7928c393c51a4a94a452cea6396c1c6ecfb2c5bf
aditya2359 <adityakumarr2359@gmail.com>
2024-10-20 14:04:34 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -18,8 +18,8 @@ def get_requirements(file_path:str)->List[str]:
 setup(
     name='DiamondPricePrediction',
     version='0.0.1',
-    author='vishal k gupta',
-    author_email='vishalgupta7571@gmail.com',
+    author='Aditya Kumar',
+    author_email='adityakumarr2359@gmail.com',
     install_requires=get_requirements('requirements.txt'),
     packages=find_packages()
 
